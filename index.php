2be686af599399ebcf4c763ba29a2f5d0e1d24f4
Ikki Takahashi <new_ittyandesu@outlook.jp>
2017-05-01 16:48:52 +0000

    Lesson #5

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
-$msg = "hello from the TOP!";
-echo $msg;
+// 定数
 
-var_dump($msg);
+define("MY_EMAIL", "test@example.com");
+
+echo MY_EMAIL;
+
+var_dump(__LINE__);
+var_dump(__FILE__);
+var_dump(__DIR__);
